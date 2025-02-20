6e85dd21b9b6ecf4d39c072e7f3a8ed3605c4457
himynamesjim <126286733+himynamesjim@users.noreply.github.com>
2025-02-20 15:13:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -122,10 +122,7 @@ if st.button("Calculate Costs"):
     }).set_properties(**{"white-space": "normal"}))
 
 
-
-
-
-    }).set_properties(**{"white-space": "normal"}))
+    ).set_properties(**{"white-space": "normal"}))
     pdf_path = generate_pdf(customer_name, billing_term, months_remaining, total_prepaid_cost, total_first_year_cost, total_updated_annual_cost, total_subscription_term_fee, data)
     with open(pdf_path, "rb") as file:
         st.download_button(label="Download PDF", data=file, file_name="coterming_report.pdf", mime="application/pdf")
